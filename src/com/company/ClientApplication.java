acc4198a2a7e43010d9e272374db178675fec369
Hugo Piso <hugopiso@yahoo.com>
2016-09-24 19:39:16 -0400

    some fix

--- src/com/company/ClientApplication.java
+++ src/com/company/ClientApplication.java
@@ -5,6 +5,11 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ClientApplication {
 
@@ -16,40 +21,53 @@ public class ClientApplication {
 
     public void scenario() {
         System.out.println("Starting scenario");
-        Command next = this.getCommand();
-        while (next != null) {
-            this.executeCommande(next);
+        List<String> commands = this.getCommands();
+
+        commands.forEach((commandToExec) -> {
+            Command command = new Command();
+            command.setCommand(commandToExec);
+
+            this.executeCommand(command);
+        });
+    }
+
+    public List<String> getCommands() {
+
+        String fileName =  "/Users/hugopiso/Projects/uqac/java_client/commandes.txt";
+        List<String> list = new ArrayList<>();
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
+
+            //br returns as stream and convert it into a List
+            list = br.lines().collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+        return list;
     }
 
-
-    public Command getCommand() {
-        //todo
-        return null;
-    }
-
-    public void executeCommande(Command command) {
-
-        Socket socket;
+    public void executeCommand(Command command) {
 
         try {
 
             //Open socket
-            socket = new Socket(InetAddress.getLocalHost(), this.port);
-            DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+            Socket socket = new Socket(InetAddress.getLocalHost(), this.port);
+            ObjectOutputStream outToServer = new ObjectOutputStream(socket.getOutputStream());
+            BufferedReader inFromServer  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             //Send command to server
-            outToServer.writeBytes(command.getCommand());
+            outToServer.writeObject(command);
+
+            //Get response
+            String response = inFromServer.readLine();
+            System.out.println("From server" + response);
 
             //close socket
             socket.close();
 
-        }catch (UnknownHostException e) {
-
-            e.printStackTrace();
-        }catch (IOException e) {
-
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
